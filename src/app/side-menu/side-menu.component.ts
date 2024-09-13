7675524d2a01513635363c3d2044b024e91b7e78
ilias-chat <ilias.chat.7@gmail.com>
2024-09-13 04:43:43 +0200

    make the menu resizable

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -9,8 +9,15 @@ import { Component } from '@angular/core';
 })
 export class SideMenuComponent {
   selected_button:number = 1;  
+  is_menu_closed:boolean = false;
 
   onMenuButtonClicked(index:number){
     this.selected_button = index;
+  }
+
+  on_btn_menu_cliked(event:Event){
+    this.is_menu_closed=!this.is_menu_closed;
+    const target= event.target as HTMLElement;
+    target.classList.toggle('rotate');
   }
 }
