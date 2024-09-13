587c9f67a199559c0e7e38a572c4448dc9140ed2
ilias-chat <ilias.chat.7@gmail.com>
2024-09-13 03:07:38 +0200

    add pagination to clients list

--- src/app/main-content/clients/clients.component.ts
+++ src/app/main-content/clients/clients.component.ts
@@ -14,20 +14,43 @@ import { ClientFormComponent } from "./client-form/client-form.component";
 export class ClientsComponent {
   private clientsService = inject(ClientsService);
   @ViewChild(ClientFormComponent) client_form_component!:ClientFormComponent;
+  all_clients!:Client[];
   clients = signal<Client[]>([]);
-  is_form_dialog_open:boolean = false;
+  current_page = signal<number>(1);
+  lines_per_page:number = 2;
+  total_pages = signal<number>(1);
 
   ngOnInit(){
-    this.clients.set(this.clientsService.all_clients());
+    this.all_clients = this.clientsService.all_clients();
+
+    this.clients.set(this.all_clients.slice(0, this.lines_per_page));
+    let total_clients = this.all_clients.length;
+    if(total_clients % this.lines_per_page == 0)
+      this.total_pages.set(total_clients/this.lines_per_page);
+    else
+      this.total_pages.set(Math.trunc(total_clients/this.lines_per_page)+1);
+  }
+
+  on_next_btn_clicked(){
+    if(this.current_page()==this.total_pages()) return;
+    this.current_page.set(this.current_page()+1);
+    let starting_index = this.lines_per_page*(this.current_page()-1);
+    let ending_index = starting_index + this.lines_per_page;
+    //console.log('start: '+starting_index,'end: '+ending_index);
+    this.clients.set(this.all_clients.slice(starting_index, ending_index));
+  }
+
+  on_previous_btn_clicked(){
+    if(this.current_page()==1) return;
+    this.current_page.set(this.current_page()-1);
+    let starting_index = this.lines_per_page*(this.current_page()-1);
+    let ending_index = starting_index + this.lines_per_page;
+    //console.log('start: '+starting_index,'end: '+ending_index);
+    this.clients.set(this.all_clients.slice(starting_index, ending_index));
   }
 
   open_form_dialog(){
-    //this.is_form_dialog_open = true;
     this.client_form_component.open_dialog();
-  }
-
-  close_form_dialog(){
-    this.is_form_dialog_open = false;
   }
 
   on_add_client(){
