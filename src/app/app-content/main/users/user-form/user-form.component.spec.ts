326f5b6a3b8f554a3f2cbb9a0d83752178cf13b3
ilias-chat <ilias.chat.7@gmail.com>
2024-10-19 17:54:59 +0200

    test

--- src/app/app-content/main/users/user-form/user-form.component.spec.ts
+++ src/app/app-content/main/users/user-form/user-form.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserFormComponent } from './user-form.component';
-import { ProfilesService } from '../profiles.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing'; // Import this
 
 describe('UserFormComponent', () => {
   let component: UserFormComponent;
@@ -9,7 +9,7 @@ describe('UserFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserFormComponent]
+      imports: [UserFormComponent, HttpClientTestingModule  ]
     })
     .compileComponents();
     
